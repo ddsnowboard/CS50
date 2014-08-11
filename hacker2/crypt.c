742d4968b3051e7aab39f9652b1211d81b2957e4
Will <ddsnowboard@gmail.com>
2014-08-11 11:53:16 -0400

    I made all the errors go away, but now there's a segfault.
    How do you know anything about those? *sigh*

--- hacker2/crypt.c
+++ hacker2/crypt.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 #include <crypt.h>
 #include <string.h>
+#include <cs50.h>
+#define _XOPEN_SOURCE
+#include <unistd.h>
 string rotate(string password);
-int main(int argc, char* argv)
+int main(int argc, char* argv[])
 {
     if(argc != 2)
     {
-        printf("You need to give the encrypted password, you fool!");
-        return 0;
+        printf("You need to give the encrypted password, you fool!\n");
+        return 1;
     }
-    string hash = argv[1];
     else
     {
-        char password[1];
-        while (crypt(password) != hash)
+        string hash = argv[1];
+        string password = "aa";
+        string salt = "aa";
+        string error = "ERROR";
+        while (crypt(password, salt) != hash && password != error)
 		{
 		    password = rotate(password);
+		    salt[0] = password[0];
+		    salt[1] = password[1];
 		}
 		printf("%s\n", password);
 		return 0;
@@ -27,7 +34,7 @@ string rotate(string password)
 	int len = strlen(password);
 	for(int i = len-1;i >= 0; i--)
 	{
-		if(password[i] >= 'a' && < 'z')
+		if(password[i] >= 'a' && password[i] < 'z')
 		{
 			password[i]++;
 			return password;
@@ -37,8 +44,9 @@ string rotate(string password)
 			password[i] = 'a';
 			if(i == 0)
 			{
-				return concat('a', password);
+				return strcat("a", password);
 			}
 		}
 	}
+	return "ERROR";
 }
