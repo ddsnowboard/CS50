7853412032f2a8d5525a2c6c26e5cdf2a40e39a3
ddsnowboard <ddsnowboard@gmail.com>
2014-08-11 10:05:40 -0500

    I think I finished it.
    I need to compile and fix all the stupids I made, and then I need to
    figure out what the deal is with salting, and then I need to maybe
    check with that words file in the spec first.

--- hacker2/crypt.c
+++ hacker2/crypt.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
 #include <crypt.h>
 #include <string.h>
+string rotate(string password);
 int main(int argc, char* argv)
 {
     if(argc != 2)
     {
-        printf("You need to give the encrypted password!");
+        printf("You need to give the encrypted password, you fool!");
         return 0;
     }
     string hash = argv[1];
     else
     {
         char password[1];
-        for(int i = 1; i<10;i++)
-        {
-            for(int j = 0; j<i; j++)
-            {
-                password[j] = 'a';
-            }
-            if(crypt(password) == hash)
-            {
-                printf("%s", password);
-                return 0;
-            }
-            // I need to figure out how to rotate the string to brute force it. Check the spec for how to deal with caps. 
-            // Maybe also run through the dictionary file first, just in case you get lucky. 
-        }
+        while (crypt(password) != hash)
+		{
+		    password = rotate(password);
+		}
+		printf("%s\n", password);
+		return 0;
     }    
+}
+
+string rotate(string password)
+{
+	int len = strlen(password);
+	for(int i = len-1;i >= 0; i--)
+	{
+		if(password[i] >= 'a' && < 'z')
+		{
+			password[i]++;
+			return password;
+		}
+		else if(password[i] == 'z')
+		{
+			password[i] = 'a';
+			if(i == 0)
+			{
+				return concat('a', password);
+			}
+		}
+	}
 }
